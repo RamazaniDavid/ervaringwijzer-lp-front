f2c12e642cf98e668b2f160cf5eb1fd58bf044ef
ProDeveloper <ramazani.david@gmail.com>
2022-06-14 19:12:14 +0430

    make map data

--- src/components/indexComponents/Businesses.tsx
+++ src/components/indexComponents/Businesses.tsx
@@ -4,7 +4,7 @@ function Businesses() {
   return (
     <section id="businesses">
       <div className="w-full bg-[#0085ff] text-xl text-white">
-        <div className="mx-auto grid h-36 w-11/12 grid-cols-3 place-items-center text-center lg:w-10/12  xl:w-9/12 xl:grid-cols-4 2xl:w-8/12">
+        <div className="mx-auto grid h-36 w-11/12 grid-cols-3 place-items-center text-center lg:w-10/12  xl:w-9/12 xl:grid-cols-4 2xl:w-6/12">
           <div className="text-[#bfe0ff]">
             <span className="mr-3 text-3xl  font-bold text-white">264</span>
             <br className="xl:hidden" />
